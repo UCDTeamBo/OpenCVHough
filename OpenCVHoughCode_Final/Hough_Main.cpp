53a16fc1e300a46b54260e0609a27b1007fcd76f
ravenlin <laraven@ucdavis.edu>
2015-04-15 14:57:06 -0700

    Cleaned up code
    Can run on video or webcam just comment or uncomment # defines at the
    top of the code

--- OpenCVHoughCode_Final/Hough_Main.cpp
+++ OpenCVHoughCode_Final/Hough_Main.cpp
@@ -1,192 +1,317 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/opencv.hpp>
 #include <stdio.h>
 #include <iostream>
 using namespace cv;
 
 
+//Initiating portions of the code: Make sure Camera_Capture is commented in order to have it run of webcam
+#define Camera_Capture
+#define Display_Results
+//#define Template_Process;
 
 
-/*int main()
+//Definitions
+vector<vector<Point> > contours;
+vector<Vec4i> hierarchy;
+vector<Vec3f> vecCircles;
+vector<Vec3f>::iterator itrCircles;
+vector<vector<Point> > contours_2;
+vector<Vec4i> hierarchy_2;
+vector<Vec3f> vecCircles_2;
+vector<Vec3f>::iterator itrCircles_2;
+
+Mat drawingFinal;
+Mat temp;
+int top_x, top_y, bottom_x, bottom_y;
+Mat drawing;
+Scalar color;
+Mat Copy;
+int count;
+int top_x_2, top_y_2, bottom_x_2, bottom_y_2, middle_x, middle_y;
+int num_templates=10;
+
+
+Mat MatchingMethod(Mat Source)
 {
+	/// Source image to display
+	Mat img_display,result;
+	Source.copyTo(img_display);
+	int i;
+	/// Create the result matrix
+	char buffer[400];
+	double minVal_Final = 0; double maxVal_Final = 0; Point minLoc_Final; Point maxLoc_Final; Point matchLoc; int rows; int columns;
+	cvtColor(Source, Source, CV_BGR2GRAY);
+	for (i = 1; i < num_templates + 1; i++){
+		Mat Template;
+		sprintf(buffer, "C:/Users/Lindsey/Documents/Visual Studio 2013/Projects/OpenCVHoughCode_Final/Templates/t%d.png", i);
+		Template = imread(buffer);
+		cvtColor(Template, Template, CV_BGR2GRAY);
+		int result_cols = Source.cols - Template.cols + 1;
+		int result_rows = Source.rows - Template.rows + 1;
 
-	Mat image_input,grey_image,color_image;
-	 
+		result.create(result_rows, result_cols, CV_32FC1);
 
-	image_input = imread("C:/Users/Lindsey/Documents/Visual Studio 2013/Projects/OpenCVHoughCode_Final/download.jpg", CV_LOAD_IMAGE_COLOR);
+		/// Do the Matching and Normalize
+		matchTemplate(Source, Template, result, CV_TM_CCOEFF);
+		normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
 
-	cvtColor(image_input, grey_image, CV_BGR2GRAY);
-	Canny(grey_image, grey_image, 90, 90);
-	cvtColor(grey_image, color_image, CV_GRAY2BGR);
+		//namedWindow("Template", WINDOW_AUTOSIZE);
+		/// Localizing the best match with minMaxLoc
+		double minVal; double maxVal; Point minLoc; Point maxLoc;
+		Point matchLoc;
+		minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+		//imshow("Template", result);
+		//waitKey(0);
+		if (maxVal > maxVal_Final)
+		{
+			maxVal_Final = maxVal;
+			minVal_Final = minVal;
+			minLoc_Final = minLoc;
+			maxLoc_Final = maxLoc;
+			rows = Template.rows;
+			columns = Template.cols;
 
+		}
 
-	vector<Vec2f> Hough_Output;
-
-	
-	namedWindow("HELLO", WINDOW_AUTOSIZE);
-	imshow("HELLO", grey_image);
-
-	waitKey(0);
-
-	HoughLines(grey_image, Hough_Output, 1, CV_PI / 180, 150, 0, 0);
-
-	std::cout << Hough_Output.size() << '\n';
-
-	//int i = 1;
-
-	for (size_t i = 0; i < Hough_Output.size(); i++)
-	{
-		//std::cout << "hi";
-		float rho = Hough_Output[i][0];
-		float theta = Hough_Output[i][1];
-
-		Point pt1, pt2;
-		double a = cos(theta);
-		double b = sin(theta);
-
-		double x0 = a*rho;
-		double y0 = b*rho;
-
-		pt1.x = cvRound(x0 + 1000 * (-b));
-		pt1.y = cvRound(y0 + 1000 * (a));
-		pt2.x = cvRound(x0 - 1000 * (-b));
-		pt2.y = cvRound(y0 - 1000 * (a));
-		line(color_image, pt1, pt2, Scalar(0, 0, 255), 3, CV_AA);
 	}
 
-	namedWindow("HELLO", WINDOW_AUTOSIZE);
-	imshow("HELLO", color_image);
-
-	waitKey(0);
-
-	return(0);
-}*/
-
-
-static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
-{
-	double dx1 = pt1.x - pt0.x;
-	double dy1 = pt1.y - pt0.y;
-	double dx2 = pt2.x - pt0.x;
-	double dy2 = pt2.y - pt0.y;
-	return (dx1*dx2 + dy1*dy2) / sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
-}
-
-
-void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& contour)
-{
-	int fontface = cv::FONT_HERSHEY_SIMPLEX;
-	double scale = 0.4;
-	int thickness = 1;
-	int baseline = 0;
-
-	cv::Size text = cv::getTextSize(label, fontface, scale, thickness, &baseline);
-	cv::Rect r = cv::boundingRect(contour);
-
-	cv::Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
-	cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255, 255, 255), CV_FILLED);
-	cv::putText(im, label, pt, fontface, scale, CV_RGB(0, 0, 0), thickness, 8);
-}
-
-int main(){
-	using namespace cv;
-
-	//DO I NEED an int detect_plus() { and a } at the end?
-
-	//Load source image
-	Mat src = imread("C:/Users/Lindsey/Documents/Visual Studio 2013/Projects/OpenCVHoughCode_Final/Blue_Cross_New.png");
-	if (src.empty())
-		return -1;
-
-	//next part in original code finds contours by converting to grayscale and using Canny
-	// Convert to grayscale
-	Mat gray;
-	cvtColor(src, gray, CV_BGR2GRAY);
-
-	/*namedWindow("HELLO", WINDOW_AUTOSIZE);
-	imshow("HELLO", gray);
-
-	waitKey(0);*/
-
-	//Convert to binary image using Canny
-	Mat bw;
-	Canny(gray, bw, 90, 90);
-
-	namedWindow("HELLO", WINDOW_AUTOSIZE);
-	imshow("HELLO", bw);
-
-	waitKey(0);
-
-	//"The array bw is now a binary image with edges from the shapes. Obtain the contours,"
-	// Find contours
-	std::vector<std::vector<Point> > contours;
-	findContours(bw.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-
-	//I don't completely understand this next section, but I think it sorts out actual contours from noise
-	// The array for storing the approximation curve
-	std::vector<Point> approx;
-
-	// We'll put the labels in this destination image
-	Mat dst = src.clone();
-
-	std::cout << contours.size() << " SIZE OF CONTOURS";
-	for (int i = 0; i < contours.size(); i++)
+	int match_method = 4;
+		/// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
+	if (match_method == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED)
 	{
-		// Approximate contour with accuracy proportional
-		// to the contour perimeter
-		approxPolyDP(
-			Mat(contours[i]),
-			approx,
-			arcLength(Mat(contours[i]), true) * 0.02,
-			true
-			);
+		matchLoc = minLoc_Final;
+	}
+	else
+	{
+		matchLoc = maxLoc_Final;
+	}
+		
+	rectangle(img_display, matchLoc, Point(matchLoc.x + columns, matchLoc.y + rows), Scalar::all(0), 2, 8, 0);
+	rectangle(result, matchLoc, Point(matchLoc.x + columns, matchLoc.y + rows), Scalar::all(0), 2, 8, 0);
+	
+	/// Show me what you got
+	
+	
+	return result;
+}
+
+int main()
+{
+
+	Mat image_input,grey_image,color_image, hsv_image, color_threshed;
+	bool Success;
+
+#ifdef Camera_Capture 
+	VideoCapture cap(0);
+#else
+	VideoCapture cap("C:/Users/Lindsey/Documents/Visual Studio 2013/Projects/OpenCVHoughCode_Final/IMG_0450.mov");
+#endif
+
+#ifdef Display_Results
+	namedWindow("Transformed", WINDOW_AUTOSIZE);
+	namedWindow("Original", WINDOW_AUTOSIZE);
+#else 
+#endif
+
+	//Infinite loop video/camera input
+	for (;;){
+
+		//Error Check Successfull camera READ 
+		Success = cap.read(image_input);
+		
+		if (!Success){
+			std::cout << "Cannot read a frame from video stream" << "\n";
+		}
+
+		//Convert to grayscale
+		cvtColor(image_input, grey_image, CV_BGR2GRAY);
+		
+		//Threshold image BINARY
+		threshold(grey_image, grey_image, 220, 255, THRESH_BINARY);
+
+		//Threshold image ORANGE_Color (run on original image)
+		inRange(image_input, Scalar(10,90,200), Scalar(140,230,250), color_threshed);
+
+		cvtColor(grey_image, color_image, CV_GRAY2BGR);
+
+		//Isolate orange colors byy finding contours then encapsulating in Boxes (Boxes are filled with white to generate region with orange)
+
+		findContours(color_threshed, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+		vector<vector<Point> > contours_poly(contours.size());
+		vector<Rect> boundRect(contours.size());
+		vector<Point2f>center(contours.size());
+		vector<float> radius(contours.size());
+
+		for (int i = 0; i < contours.size(); i++)
+		{
+			approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
+			boundRect[i] = boundingRect(Mat(contours_poly[i]));
+		}
+
 
 		
-		// Skip small or non-convex objects 
-		/*if (std::fabs(contourArea(contours[i])) < 100 || !isContourConvex(approx))
-			continue;*/
+		grey_image.copyTo(drawing);
+		threshold(drawing, drawing, 256, 256, THRESH_BINARY);
 
-
-		//"The vector approx now contains the vertices of the polygonal approximation for the contour.
-		//We will use its size to determine" the shape of the contour.
-
-		// num vertices of the current contour
-		int vtc = approx.size();
-
-		// Get the degree (in cosines) of all vertices
-		//MAY BE A PROBLEM IF THE INNER VERTICES READ AS 270 DEG AND NOT 90 DEG
-		std::vector<double> cos;
-		for (int j = 2; j < vtc + 1; j++)
-			cos.push_back(angle(approx[j%vtc], approx[j - 2], approx[j - 1]));
-
-		// Sort ascending the corner degree values
-		std::sort(cos.begin(), cos.end());
-
-		// Get the lowest and the highest degree
-		double mincos = cos.front();
-		double maxcos = cos.back();
-
-		//detect a plus and labels it
-		//NOT SURE IF THIS vtc 8 OR 12, depends on if inner vertices count
-		// -0.1 is just below cos(90), 0.3 is a ways above cos(90), I think can be lowered to 0.1 if inaccurate
-		//setLabel(dst, "X", contours[i]);
-		//std::cout << "HI";
-		if (vtc >= 8 /*&& mincos >= -0.1 && maxcos <= 0.3*/)
+		for (int i = 0; i< contours.size(); i++)
 		{
-			setLabel(dst, "X", contours[i]);
+			Scalar color = Scalar(255, 255, 255);
+			drawContours(drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+			top_x = boundRect[i].tl().x;
+			top_y = boundRect[i].tl().y;
+			bottom_x = boundRect[i].br().x;
+			bottom_y = boundRect[i].br().y;
+			rectangle(drawing, Point(top_x - 5, top_y - 5), Point(bottom_x + 5, bottom_y + 5) , color, CV_FILLED, 8, 0);
+			
+		}
+		
+
+		//Combine the black and white and orange region image to find region with cross
+		bitwise_and(drawing, grey_image, drawingFinal);
+
+		
+		//Eliminate noise within the image NEED TO ADD CENTER PROTOCOL 
+		erode(drawingFinal, drawingFinal, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		//dilate(drawingFinal, drawingFinal, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+
+		//Isolate white regions in the resulting image and find the largest white region to find cross
+		
+		drawingFinal.copyTo(Copy);
+		cvtColor(drawingFinal,drawingFinal, CV_GRAY2BGR);
+		//Canny(Copy, Copy, 60, 60);
+		findContours(Copy, contours_2, hierarchy_2, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+		vector<vector<Point> > contours_poly_2(contours_2.size());
+		vector<Rect> boundRect_2(contours_2.size());
+		vector<Point2f>center_2(contours_2.size());
+		vector<float>radius_2(contours_2.size());
+
+		for (int i = 0; i < contours_2.size(); i++)
+		{
+			approxPolyDP(Mat(contours_2[i]), contours_poly_2[i], 3, true);
+			boundRect_2[i] = boundingRect(Mat(contours_poly_2[i]));
 		}
 
-		else
-		{
-			setLabel(dst, "?", contours[i]);
+		int max_size = 0;
+		int box_size = 0;
+		int center_x=0, center_y=0;
+		//std::cout << "RRRG" << contours_2.size() << "\n";
+
+		if (contours_2.size() == 0){
+			continue; //COULDNT FIND CROSS!!!
 		}
+
+		count = 0;
+		for (int i = 0; i< contours_2.size(); i++)
+		{
+			color = Scalar(80, 0, 80);
+			//drawContours(drawingFinal, contours_poly_2, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+			top_x_2 = boundRect_2[i].tl().x;
+			top_y_2 = boundRect_2[i].tl().y;
+			bottom_x_2 = boundRect_2[i].br().x;
+			bottom_y_2 = boundRect_2[i].br().y;
+			box_size = abs(bottom_x_2 - top_x_2)*abs(top_y_2 - bottom_y_2);
+			middle_x = (top_x_2 + bottom_x_2) / 2;
+			middle_y = (top_y_2 + bottom_y_2) / 2;
+			if (box_size > max_size)
+			{
+				max_size = box_size;
+				center_x = middle_x;
+				center_y = middle_y;
+				
+			}
+			else
+			{
+				//Needs to be done -- Take average of blobs
+			}
+			
+		}
+
+		circle(drawingFinal, Point(center_x, center_y), 32, Scalar(0, 255, 0), 1, 8);
+		
+#ifdef Display_Results
+		imshow("Transformed", drawingFinal);
+		imshow("Original", image_input);
+#endif
+
+#ifdef Camera_Capture 
+		waitKey(1);
+#else
+		waitKey(0);
+#endif
+		
+		//imwrite("C:/Users/Lindsey/Documents/Visual Studio 2013/Projects/OpenCVHoughCode_Final/HSV_Out.png", image_input);
+
+#ifdef Template_Process
+		Mat results;
+		results = MatchingMethod(image_input);
+#ifdef Display_Results
+		namedWindow("Template", WINDOW_AUTOSIZE);
+		imshow("Template", results);
+#endif
+#endif
+
+#ifdef Camera_Capture
+#else
+		char k;
+		k = cvWaitKey(0);
+		if (k == 'q')
+		{ 
+			break; 
+		}
+#endif
+
+		//garbage Keeping 
+		contours_poly.clear();
+		boundRect.clear();
+		center.clear();
+		radius.clear();
+
+		contours_poly_2.clear();
+		boundRect_2.clear();
+		center.clear();
+		radius.clear();
+
+
 	}
-
-	//Display both src and dst images
-	imshow("src", src);
-	imshow("dst", dst);
-	waitKey(0);
-	return 0;
-
+	return(0);
 }
+
+
+
+
+/*HoughLines(grey_image, Hough_Output, 1, CV_PI / 180, 50, 0, 0);
+
+std::cout << Hough_Output.size() << '\n';
+
+
+//int i = 1;
+
+for (size_t i = 0; i < Hough_Output.size(); i++)
+{
+//std::cout << "hi";
+float rho = Hough_Output[i][0];
+float theta = Hough_Output[i][1];
+
+Point pt1, pt2;
+double a = cos(theta);
+double b = sin(theta);
+
+double x0 = a*rho;
+double y0 = b*rho;
+
+pt1.x = cvRound(x0 + 1000 * (-b));
+pt1.y = cvRound(y0 + 1000 * (a));
+pt2.x = cvRound(x0 - 1000 * (-b));
+pt2.y = cvRound(y0 - 1000 * (a));
+line(color_image, pt1, pt2, Scalar(0, 0, 255), 3, CV_AA);
+}*/
+
+//namedWindow("HELLO", WINDOW_AUTOSIZE);
+//imwrite("C:/Users/Lindsey/Documents/Visual Studio 2013/Projects/OpenCVHoughCode_Final/light_output.png", color_image);
+//imshow("HELLO", color_image);
